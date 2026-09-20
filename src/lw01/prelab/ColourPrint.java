74c21eac725a79937734173c4ac0d38af6a73122
rosdianiadiningsih <rosdianidiningsih@gmail.com>
2026-09-20 22:57:27 +0700

    Labwork 01 - Prelab

---
+++ src/lw01/prelab/ColourPrint.java
@@ -0,0 +1,30 @@
+package lw01.prelab;
+
+public class ColourPrint extends PrintJob {
+
+    private static final int TIER_LIMIT = 10;
+    private static final int TIER1_RATE = 1500;
+    private static final int TIER2_RATE = 1000;
+    private static final int SETUP_FEE = 2000;
+
+    public ColourPrint(String id, int pages) {
+        super(id, pages);
+    }
+
+    @Override
+    public int calculateCharge() {
+        int pages = getPages();
+        int pageCost;
+        if (pages <= TIER_LIMIT) {
+            pageCost = pages * TIER1_RATE;
+        } else {
+            pageCost = (TIER_LIMIT * TIER1_RATE) + ((pages - TIER_LIMIT) * TIER2_RATE);
+        }
+        return pageCost + SETUP_FEE;
+    }
+
+    @Override
+    public String label() {
+        return "Colour";
+    }
+}
